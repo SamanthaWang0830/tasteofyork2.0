73c3ee155ae1e4dcf165c68c8bc8d75250619909
zijingw <109635814+SamanthaWang0830@users.noreply.github.com>
2023-10-11 15:46:57 -0400

    added some tests

--- update-tasteofyork/src/pages/Register/RegisterPage.test.tsx
+++ update-tasteofyork/src/pages/Register/RegisterPage.test.tsx
@@ -3,25 +3,27 @@ import { render, screen , fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { Provider } from 'react-redux'; 
 import store from '../../store/store';
-import LoginPage from './LoginPage';
+import RegisterPage from './RegisterPage';
 import { BrowserRouter } from 'react-router-dom'
 
 
-describe("Login",()=>{
-    it('it shows two inputs and a button', async()=>{
+describe("Register",()=>{
+    it('it shows two text inputs and one password input', async()=>{
        // Render the component
         render(
             <BrowserRouter>
                 <Provider store={store}>
-                    <LoginPage/>
+                    <RegisterPage/>
                 </Provider>
             </BrowserRouter>
         )
        // Manipulate the component or find an element in it
-        const inputs= screen.getAllByRole('textbox')
+        const textInputs= screen.getAllByRole('textbox')
+        const passwordInput = screen.getByPlaceholderText('Password');
 
        // Assertion- make sure the component is doing what we expect it to do
-       expect(inputs).toHaveLength(1)
+       expect(textInputs).toHaveLength(2)
+       expect(passwordInput).toBeInTheDocument();
     }),
 
     it('shows error message when email is empty', async()=>{
@@ -29,12 +31,12 @@ describe("Login",()=>{
         const {getByRole, getByText } = render(
              <BrowserRouter>
                  <Provider store={store}>
-                     <LoginPage/>
+                     <RegisterPage/>
                  </Provider>
              </BrowserRouter>
         )
         // Manipulate the component or find an element in it
-        const loginButton = getByRole('button', { name: 'Login' });
+        const loginButton = getByRole('button', { name: 'Register' });
         fireEvent.click(loginButton);
         const errorMessage = getByText('Please enter a valid email');
         // Assertion- make sure the component is doing what we expect it to do
@@ -42,21 +44,21 @@ describe("Login",()=>{
 
     }),
 
-    it('shows error message when password is wrong', async()=>{
+    it('shows error message when password is invalid', async()=>{
         const {getByRole, getByText,getByPlaceholderText } = render(
             <BrowserRouter>
                 <Provider store={store}>
-                    <LoginPage/>
+                    <RegisterPage/>
                 </Provider>
             </BrowserRouter>
         )
 
         const emailInput = getByPlaceholderText('Email');
         const passwordInput = getByPlaceholderText('Password');
-        const loginButton = getByRole('button', { name: 'Login' });
+        const registerButton = getByRole('button', { name: 'Register' });
         fireEvent.change(emailInput, { target: { value: 'test@test.com' } });
         fireEvent.change(passwordInput, { target: { value: 'sdsas1234' } });
-        fireEvent.click(loginButton);
+        fireEvent.click(registerButton);
 
         const errorMessage = getByText('Password should contains minimum 1 character for both uppercase and lowercase letter')
         expect(errorMessage).toBeInTheDocument()
